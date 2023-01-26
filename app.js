f2469fd84ba52a3c81d7f433d00d74529b29e6fb
sheebukhan62 <sheebukhan62@gmail.com>
2023-01-26 23:24:26 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.3,
+    image1: '/Liq_1.jpg',
+    image2: '/Liq_2.jpg',
+    // angle: Math.PI / 4,
+    speedIn: 2,
+    speedOut: 5,
+    // displacementImage: '/dis.png'
+    displacementImage: '/stripe1.png'
+
+})
